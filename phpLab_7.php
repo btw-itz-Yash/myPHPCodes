ebbf14b0da4a3cf094ffbee41bae15e2d4aac779
btw_itz_Yash <yashgangwar.ind@gmail.com>
2023-02-12 00:17:43 +0530

    new program added

--- phpLab_7.php
+++ phpLab_7.php
@@ -4,19 +4,13 @@
     //exit controlled loop --> do while loop
     echo "<table style='border:2px solid red;background-color:blue'>";
     echo "<tr >";
-    $temp = 0;
-    for($count = 1 ; $count<=10;$count++){
-        for ($count_c = 1; $count_c <= 10; $count_c++) {
-            if($count==1){
-                echo "<td style='border:2px solid black;height:45px;width:45px;background-color:white'>0$temp</td>";
-                $temp++;
-            }
-            else{
-                echo "<td style='border:2px solid black;height:45px;width:45px; background-color:white'>$temp</td>";
-                $temp++;
-            }  
-        }  
-        echo "<tr>";
+    
+    for($count = 2 ; $count<=20;$count++){
+    for ($count_c = 1; $count_c <= 10; $count_c++) {
+        $temp = $count * $count_c;
+        echo "<td style='border:2px solid black;height:45px;width:45px;background-color:white'>$count*$count_c=$temp</td>";
+    }
+            echo "<tr>";
     }
     echo "</tr></table>";
 ?>
